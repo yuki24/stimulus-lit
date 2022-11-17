2d805ecd0969ee124772e45e275c4b1c2178943b
Yuki Nishijima <yuki24@hey.com>
2022-11-18 00:00:01 +0900

    Remove playground

--- playground/index.js
+++
@@ -1,7 +0,0 @@
-import { Application } from '@hotwired/stimulus'
-import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers'
-
-const application = Application.start()
-const context = require.context('./controllers', true, /\.js$/)
-
-application.load(definitionsFromContext(context))
